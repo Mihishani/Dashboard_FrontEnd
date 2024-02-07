6b9d824104bd31b3338f98412509c4d79671d745
Your Name <you@example.com>
2024-02-07 19:08:38 +0530

    Update Dashboard Components Icon

--- src/components/HeaderTop.tsx
+++ src/components/HeaderTop.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { ImWink2 } from "react-icons/im";
 import { IoIosSpeedometer } from "react-icons/io";
+import { IoMdSettings } from "react-icons/io";
 
 
 const HeaderTop = ()=>{
     // @ts-ignore
     return(
-        <div>
+        <div className={'bg-gray-950'}>
             <div className={'flex-wrap items-center justify-center bg-gradient-to-b from-blue-500 to-blue-700 h-[1000px] w-[220px] '}>
                 <ImWink2 className={'icon relative left-6 top-8'}/>
                 <div className={"SBAdmin text-xl font-sans  font-bold text-white relative top-0 left-[80px]"}>SB ADMIN<sup>2</sup></div>
@@ -23,6 +24,21 @@ const HeaderTop = ()=>{
                 <div className={"sidebar-heading text-gray-300 text-xs font-bold relative top-12 left-2"}>
                     INTERFACE
                 </div>
+
+                <li className={"nav-item flex "}>
+                    <a className={"nav-link collapsed text-gray-300 hover:text-white"} href="#" data-toggle="collapse" data-target="#collapseTwo"
+                       aria-expanded="true" aria-controls="collapseTwo">
+                        <IoMdSettings className={'icon1   relative h-8 w-4 top-16 left-5'}/>
+                        <span className={'text-[15px] relative top-9 left-10'}>Components</span>
+                    </a>
+                    <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                        <div className="bg-white py-2 collapse-inner rounded">
+                            <h6 className="collapse-header">Custom Components:</h6>
+                            <a className="collapse-item" href="buttons.html">Buttons</a>
+                            <a className="collapse-item" href="cards.html">Cards</a>
+                        </div>
+                    </div>
+                </li>
 
             </div>
 
