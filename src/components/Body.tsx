7f995293a96d060f7a23937479ebc5f56c71a739
Your Name <you@example.com>
2024-02-08 11:55:42 +0530

    add cards tailwind css

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { FaDownload } from "react-icons/fa";
 import { FaCalendar } from "react-icons/fa";
+import { FaDollarSign } from "react-icons/fa";
+import { FaClipboardList } from "react-icons/fa";
+import { FaMessage } from "react-icons/fa6";
 
 const Body =()=>{
     return(
@@ -14,10 +17,10 @@ const Body =()=>{
                  Generate  Report
             </button>
 
-            <div className="row ">
+            <div className="row inline-flex gap-2 relative top-5 left-1">
 
 
-                <div className={" col-span-3 md:col-span-6 mb-4 bg-white rounded w-[300px] relative left-10 top-4"}>
+             {/*   <div className={" col-span-3 md:col-span-6 mb-4 bg-white rounded w-[300px] relative top-4"}>
                     <div className={"card  border-l-4 border-blue-600  rounded-l-lg shadow h-100 py-6"}>
                         <div className={"card-body"}>
                             <div className={"row flex gap-0 items-center"}>
@@ -32,19 +35,73 @@ const Body =()=>{
                             </div>
                         </div>
                     </div>
-                </div>
+                </div>*/}
 
-                <div className="col-span-3 md:col-span-6 mb-4 bg-white rounded w-[300px]">
-                    <div className="card border-left-success shadow h-100 py-2">
+                <div className="col-span-3 md:col-span-6 mb-4 bg-white rounded w-[300px] relative left-10 top-4">
+                    <div className="card  border-l-4 rounded-l-lg border-blue-500 shadow h-100 py-6">
                         <div className="card-body">
-                            <div className="row no-gutters align-items-center">
+                            <div className="row flex gap-0 items-center">
                                 <div className="col mr-2">
-                                    <div className="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                        Earnings (Annual)</div>
-                                    <div className="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                    <div className="text-xs font-semibold text-primary uppercase text-blue-500 mb-1 relative left-6">
+                                        Earnings (Monthly)</div>
+                                    <div className="text-lg mb-0 font-bold text-gray-600 relative left-6">$40,000</div>
                                 </div>
                                 <div className="col-auto">
-                                    <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                    <FaCalendar  className={'text-gray-300 w-10 h-7 relative left-20'}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-span-3 md:col-span-6 mb-4 bg-white rounded w-[300px] relative left-10 top-4">
+                    <div className="card  border-l-4 rounded-l-lg border-green-500 shadow h-100 py-6">
+                        <div className="card-body">
+                            <div className="row flex gap-0 items-center">
+                                <div className="col mr-2">
+                                    <div className="text-xs font-semibold text-primary uppercase text-green-500 mb-1 relative left-6">
+                                        Earnings (Annual)</div>
+                                    <div className="text-lg mb-0 font-bold text-gray-600 relative left-6">$215,000</div>
+                                </div>
+                                <div className="col-auto">
+                                    <FaDollarSign  className={'text-gray-300 w-10 h-7 relative left-20'}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-span-3 md:col-span-6 mb-4 bg-white rounded w-[300px] relative left-10 top-4">
+                    <div className="card  border-l-4 rounded-l-lg border-cyan-500 shadow h-100 py-6">
+                        <div className="card-body">
+                            <div className="row flex gap-0 items-center">
+                                <div className="col mr-2">
+                                    <div className="text-xs font-semibold text-primary uppercase text-cyan-500 mb-1 relative left-6">
+                                    Task</div>
+                                    <div className="text-lg mb-0 font-bold text-gray-600 relative left-6">50%</div>
+                                </div>
+                                <div className="col-auto">
+                                    <FaClipboardList  className={'text-gray-300 w-10 h-7 relative left-20'}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+                <div className="col-span-3 md:col-span-6 mb-4 bg-white rounded w-[300px] relative left-10 top-4">
+                    <div className="card  border-l-4 rounded-l-lg border-yellow-500 shadow h-100 py-6">
+                        <div className="card-body">
+                            <div className="row flex gap-0 items-center">
+                                <div className="col mr-2">
+                                    <div className="text-xs font-semibold text-primary uppercase text-yellow-500 mb-1 relative left-6">
+                                        Pending Request</div>
+                                    <div className="text-lg mb-0 font-bold text-gray-600 relative left-6">18</div>
+                                </div>
+                                <div className="col-auto">
+                                    <FaMessage  className={'text-gray-300 w-10 h-7 relative left-20'}/>
                                 </div>
                             </div>
                         </div>
