c373e797b782f31fed7c885b89e76d2664fc7ab3
Your Name <you@example.com>
2024-02-08 07:57:27 +0530

    Update search bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,9 @@ const Header = ()=>{
 
     return(
         <div className={'absolute text-blue-300 bg-white h-[80px] w-full '}>
-             <div className={'w-[50px] sm:w-[50px] md:w-[30%] relative justify-between items-center top-5 bottom-5 left-[250px]  '}>
+             <div className={'w-[50px] sm:w-[50px] md:w-[450px] relative justify-between items-center top-5 bottom-5 left-[250px]  '}>
                  <input type={'text'}  placeholder={'Search for...'} className={'border-gray-200 border p-2 px-4 rounded-lg w-full' }/>
-                 <button className={'bg-blue-500 hover:bg-blue-300   relative w-[41px] h-[41.5px] top-[-41.7px] rounded-r-lg left-[414.5px] fixed'}>
+                 <button className={'bg-blue-500 hover:bg-blue-300   relative w-[41px] h-[41.5px] top-[-41.7px] rounded-r-lg left-[414.5px] '}>
                      <FaSearch className={'relative right-1 left-3 text-white fixed'}/>
                  </button>
 
